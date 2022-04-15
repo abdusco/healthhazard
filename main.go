e8cdd9cda456c5562608b472e5145f8edfb99a6c
Abdussamet Kocak <abdus@abdus.dev>
2022-04-15 17:22:59 +0300

    feat: Use Echo for cross-cutting concerns

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"log"
 	"net/http"
 	"os"
@@ -19,6 +21,11 @@ func main() {
 	upstreamHealthcheckPath := requireEnv("UPSTREAM_HEALTHCHECK_PATH")
 	upstreamTimeout := requireEnvDuration("UPSTREAM_TIMEOUT")
 	upstreamHealth := fmt.Sprintf("http://localhost:%s%s", upstreamPort, upstreamHealthcheckPath)
+
+	e := echo.New()
+	e.HideBanner = true
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	signals := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
@@ -41,35 +48,32 @@ func main() {
 		}
 	}()
 
-	http.HandleFunc(healthcheckPath, func(w http.ResponseWriter, r *http.Request) {
+	e.GET(healthcheckPath, func(c echo.Context) error {
 		if terminating {
-			w.WriteHeader(http.StatusServiceUnavailable)
-			w.Write([]byte("terminating"))
-			return
+			return c.String(http.StatusServiceUnavailable, "terminating")
 		}
 
 		client := http.Client{
 			Timeout: upstreamTimeout,
 		}
+
+		log.Printf("checking upstream health at %s", upstreamHealth)
 		res, err := client.Get(upstreamHealth)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("upstream service is not available"))
-			return
+			log.Print("failed to call upstream")
+			return c.String(http.StatusServiceUnavailable, "upstream service is not available")
 		}
 		if res.StatusCode >= http.StatusInternalServerError {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("upstream service is returning error"))
-			return
+			log.Printf("upstream returned error: %d", res.StatusCode)
+			return c.String(http.StatusServiceUnavailable, "upstream service is returning error")
 		}
 
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+		log.Print("upstream is healthy")
+		return c.String(http.StatusOK, "OK")
 	})
 
 	go func() {
-		log.Printf("listening healthcheck requests at :%s", port)
-		log.Fatal(http.ListenAndServe(":"+port, nil))
+		log.Fatal(e.Start(":" + port))
 	}()
 
 	<-done
